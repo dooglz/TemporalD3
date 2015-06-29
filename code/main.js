4ea7897d5a41386b6caf0243ef2e8ce153e863b9
Sam Serrels <doogly1@gmail.com>
2015-06-29 15:31:32 +0100

    Refactoring code out of main.js into method_simple

--- code/main.js
+++ code/main.js
@@ -1,135 +1,88 @@
 /// <reference path="../typings/d3/d3.d.ts"/>
 /// <reference path="../typings/jquery/jquery.d.ts"/>
+//######################################################################
+//########    Date slider (more complex than it looks)
+//######################################################################
 var startDate = new Date("2007");
 var endDate = new Date("2016");
 var selectedDate = new Date("2011");
-
-var filteredLinks = [];
-var unfilteredLinks = [];
-var data;
-var node;
-var link;
 
 //------------------------------------------
 var diffY = endDate.getFullYear() - startDate.getFullYear() + 1;
 var tickvals = [];
 var ticknames = [];
+var canvasWidth = $('#chart').width();
+var canvasHeight = $('#chart').height();
+
 for (var i = 0; i < diffY; i++) {
-  tickvals[i] = i * 12;
-  ticknames[i] = "" + (startDate.getFullYear() + i);
+    tickvals[i] = i * 12;
+    ticknames[i] = "" + (startDate.getFullYear() + i);
 }
 
 $('#ex1').slider({
-  formatter: function (value) {
-    return 'Current value: ' + value;
-  }
+    formatter: function(value) {
+        return 'Current value: ' + value;
+    }
 });
 
 function slided() {
-  Update();
+    Update();
 }
 
 var dateSlider = $("#ex13").slider({
-  ticks: tickvals,
-  ticks_labels: ticknames,
-  ticks_snap_bounds: 0,
-  min: 0,
-  max: diffY * 12,
-  step: 1,
-  value: 2,
-  formatter: function (value) {
-    var dd = new Date(startDate.toUTCString());
-    dd.setMonth(dd.getMonth() + value);
-    return dd.toDateString().slice(4);
-  },
+    ticks: tickvals,
+    ticks_labels: ticknames,
+    ticks_snap_bounds: 0,
+    min: 0,
+    max: diffY * 12,
+    step: 1,
+    value: 2,
+    formatter: function(value) {
+        var dd = new Date(startDate.toUTCString());
+        dd.setMonth(dd.getMonth() + value);
+        return dd.toDateString().slice(4);
+    },
 }).on('slide', slided).data('slider');
 dateSlider.setValue(48);
+
+//######################################################################
+//########    Main Update functions
+//######################################################################
+
 Update();
+var graphdata;
 
 function Update() {
-  selectedDate = new Date(startDate.toUTCString());
-  selectedDate.setMonth(selectedDate.getMonth() + dateSlider.getValue());
-  //Do we have data?
-  if (data === undefined) {
-    console.log("Loading Data");
-    //No, load it and bail out
-    d3.json("graph-byyear.json", function (error, graph) {
-      data = graph;
-      if (error) {
-        console.error(error);
-      } else {
-        Update();
-      }
-    });
-    return;
-  }
-  
-  //we have data, filter it.
-  unfilteredLinks = data.links;
-  filteredLinks = data.links.filter(
-    function (d) {
-      //console.log(d.date);
-      //return true;
-      return selectedDate >= new Date(d.date);
-    });
-    
-  //Create Graph
-  link = svg.selectAll("line").data(filteredLinks);
-  link.enter().append("line")
-    .style("stroke", function (d) {
-    //will break if  > 20 years in scale
-    return d3.rgb(fill(parseInt(d.date.slice(0, 4)) - startDate.getFullYear())).darker();
-  });
-  link.exit().remove();
-
-  node = svg.selectAll("circle").data(data.nodes);
-  node.enter().append("circle")
-    .attr("r", radius - .75)
-    .style("fill", function (d) { return fill(d.group); })
-    .style("stroke", function (d) { return d3.rgb(fill(d.group)).darker(); })
-    .call(force.drag);
-  node.exit().remove();
-
-  //restart simulation
-  //  force.stop();
-  force
-    .nodes(data.nodes)
-    .links(filteredLinks)
-    .on("tick", tick)
-    .start();
+    selectedDate = new Date(startDate.toUTCString());
+    selectedDate.setMonth(selectedDate.getMonth() + dateSlider.getValue());
+    //Do we have data?
+    if (graphdata === undefined) {
+        console.log("Loading Data");
+        //No, load it and bail out
+        d3.json("graph-byyear.json", function(error, graph) {
+            graphdata = graph;
+            if (error) {
+                console.error(error);
+            } else {
+                console.log("Loaded");
+                Update();
+            }
+        });
+        return;
+    }
+    selected_method.SetData(graphdata);
+    selected_method.SetDate(selectedDate);
+    //we have data
+    selected_method.Update();
 }
 
-$(window).resize(function () {
-  width = $('#chart').width();
-  height = $('#chart').height();
-});
+$(window).resize(resize);
 
-var width = $('#chart').width(),
-  height = $('#chart').height(),
-  radius = 6;
-
-var fill = d3.scale.category20();
-
-var force = customLayout()
-  .gravity(.28)
-  .charge(-640)
-  .linkDistance(50)
-  .size([width, height]);
-
-var svg = d3.select("#chart").append("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-function tick() {
-  node.attr("cx", function (d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
-    .attr("cy", function (d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); });
-
-  link.attr("x1", function (d) { return d.source.x; })
-    .attr("y1", function (d) { return d.source.y; })
-    .attr("x2", function (d) { return d.target.x; })
-    .attr("y2", function (d) { return d.target.y; });
+function resize() {
+    canvasWidth = $('#chart').width();
+    canvasHeight = $('#chart').height();
+    selected_method.Redraw(canvasWidth, canvasHeight);
 }
-
 
 //######################################################################
 //########    Method Picking, Validating, Loading
@@ -143,122 +96,131 @@ changeMethod(m_simple);
 //dropdown selector ------------
 $("#methodpicker").html("");
 for (i = 0; i < methods.length; i++) {
-  $("#methodpicker").append("<option>" + methods[i].name + "</option>");
+    $("#methodpicker").append("<option>" + methods[i].name + "</option>");
 }
-$("#methodpicker").on('change', function () {
-  changeMethod($("#methodpicker").val());
+$("#methodpicker").on('change', function() {
+    changeMethod($("#methodpicker").val());
 });
 
 function VerifyMethodParmeters(method) {
-  var mustHaveParams = ["name", "ptype", "pval"];
-  var acceptedParams = ["slider", "checkbox", "textbox"];
+    var mustHaveParams = ["name", "ptype", "pval"];
+    var acceptedParams = ["slider", "checkbox", "textbox"];
 
-  for (var i in method.parameters) {
-    var param = method.parameters[i];
-    for (var j in mustHaveParams) {
-      var str = mustHaveParams[j];
-      if (!param.hasOwnProperty(str)) {
-        console.error("Method '" + method.name + "', parameter '" + param.name + "', must have a '" + str + "' member!");
-        return false;
-      }
+    for (var i in method.parameters) {
+        var param = method.parameters[i];
+        for (var j in mustHaveParams) {
+            var str = mustHaveParams[j];
+            if (!param.hasOwnProperty(str)) {
+                console.error("Method '" + method.name + "', parameter '" + param.name + "', must have a '" + str + "' member!");
+                return false;
+            }
+        }
+
+        if ($.inArray(param.ptype, acceptedParams) == -1) {
+            console.error("Method '" + method.name + "', Unkown parameter type : " + param.name + " - " + param.ptype);
+        }
+
     }
-
-    if ($.inArray(param.ptype, acceptedParams) == -1) {
-      console.error("Method '" + method.name + "', Unkown parameter type : " + param.name + " - " + param.ptype);
-    }
-
-  }
-  return true;
+    return true;
 }
 
 function changeMethod(methodName) {
-  if(typeof(methodName) === "string"){
-    var find = $.grep(methods, function (e) { return e.name == methodName });
-    if (find.length == 1) {
-      selected_method = find[0];
-    } else if (find.length > 1) {
-      console.error("Multiple methods registered with the name:" + methodName);
-      return;
+    if (typeof(methodName) === "string") {
+        var find = $.grep(methods, function(e) {
+            return e.name == methodName
+        });
+        if (find.length == 1) {
+            selected_method = find[0];
+        } else if (find.length > 1) {
+            console.error("Multiple methods registered with the name:" + methodName);
+            return;
+        } else {
+            console.error("Can't find method with name: " + methodName);
+            return;
+        }
     } else {
-      console.error("Can't find method with name: " + methodName);
-      return;
+        if (!methodName.hasOwnProperty("name")) {
+            console.error("Unkown object type passed to changeMethod");
+            return;
+        }
+        selected_method = methodName;
     }
-  }else{
-    if(!methodName.hasOwnProperty("name")){
-      console.error("Unkown object type passed to changeMethod");
-      return;
+    resize();
+    selected_method.SetDateBounds(startDate, endDate);
+
+    //clear param div
+    var pdiv1 = $('#paramDiv1').html("");
+    var pdiv2 = $('#paramDiv2').html("");
+    var pdiv;
+
+    console.log("Loading Method: " + selected_method.name);
+    if (!selected_method.hasOwnProperty("parameters")) {
+        console.log("Method: " + selected_method.name + " has no parameters");
+        return;
     }
-    selected_method = methodName;
-  }
-  
-  //clear param div
-  var pdiv1 = $('#paramDiv1').html("");
-  var pdiv2 = $('#paramDiv2').html("");
-  var pdiv;
-  
-  console.log("Loading Method: " + selected_method.name);
-  if (!selected_method.hasOwnProperty("parameters")) {
-    console.log("Method: " + selected_method.name + " has no parameters");
-    return;
-  }
-  
-  //verify parameters
-  if (!VerifyMethodParmeters(selected_method)) { return; }
 
-  for (var i in selected_method.parameters) {
-    //toggle placement div
-    if(pdiv == pdiv1){pdiv = pdiv2;}else{pdiv = pdiv1;}
-    var param = selected_method.parameters[i];
-    var newdiv = $("<div class='methodParam'/>");
-    switch (param.ptype) {
-
-      case "slider":
-        newdiv.append("<p>" + param.name + "</p>");
-        var sliderDiv = $("<input id=" + param.name + " data-slider-id='ex1Slider' type='text' data-slider-min=" + param.minval + " data-slider-max=" + param.maxval + " data-slider-step=" + param.step + " data-slider-value=" + param.pval + " />");
-        newdiv.append(sliderDiv);
-        sliderDiv.slider();
-        //register calback using some legit hax
-        !function outer(pp) {
-          sliderDiv.change('slide', function inner(e) {
-            pp.pval = e.value.newValue;
-            selected_method.paramChanged(pp);
-          });
-        } (param);
-        break;
-
-      case "checkbox":
-        var boxDiv = $("<input  id=" + param.name + " type='checkbox' " + (param.pval ? "checked" : "") + ">");
-        newdiv.append(boxDiv).append("  " + param.name);
-        !function outer(pp, bb) {
-          boxDiv.change(function inner() {
-            pp.pval = bb.is(":checked");
-            selected_method.paramChanged(pp);
-          });
-        } (param, boxDiv);
-        break;
-
-      case "textbox":
-        newdiv.append("<p>" + param.name + "</p>");
-        var igroup = $("<div class='input-group'></div>");
-        var input = $("<input type='text class='form-control'>");
-        igroup.append(input);
-        var span = $("<span class=input-group-btn'/>");
-        var btn = $("<button class='btn btn-default btn-sm' type='button'>Go!</button>");
-        newdiv.append(igroup.append(span.append(btn)));
-        !function outer(pp, bb) {
-          btn.click(function inner() {
-            pp.pval = bb.val();
-            selected_method.paramChanged(pp);
-          });
-        } (param, input);
-        break;
-
-      default:
-        console.error("Unkown method parameter type : " + param.name + " - " + param.ptype);
-        break;
+    //verify parameters
+    if (!VerifyMethodParmeters(selected_method)) {
+        return;
     }
-    pdiv.append(newdiv);
-  }
-  pdiv.hide().show(0);
 
+    for (var i in selected_method.parameters) {
+        //toggle placement div
+        if (pdiv == pdiv1) {
+            pdiv = pdiv2;
+        } else {
+            pdiv = pdiv1;
+        }
+        var param = selected_method.parameters[i];
+        var newdiv = $("<div class='methodParam'/>");
+        switch (param.ptype) {
+
+            case "slider":
+                newdiv.append("<p>" + param.name + "</p>");
+                var sliderDiv = $("<input id=" + param.name + " data-slider-id='ex1Slider' type='text' data-slider-min=" + param.minval + " data-slider-max=" + param.maxval + " data-slider-step=" + param.step + " data-slider-value=" + param.pval + " />");
+                newdiv.append(sliderDiv);
+                sliderDiv.slider();
+                //register calback using some legit hax
+                ! function outer(pp) {
+                    sliderDiv.change('slide', function inner(e) {
+                        pp.pval = e.value.newValue;
+                        selected_method.ParamChanged(pp);
+                    });
+                }(param);
+                break;
+
+            case "checkbox":
+                var boxDiv = $("<input  id=" + param.name + " type='checkbox' " + (param.pval ? "checked" : "") + ">");
+                newdiv.append(boxDiv).append("  " + param.name);
+                ! function outer(pp, bb) {
+                    boxDiv.change(function inner() {
+                        pp.pval = bb.is(":checked");
+                        selected_method.ParamChanged(pp);
+                    });
+                }(param, boxDiv);
+                break;
+
+            case "textbox":
+                newdiv.append("<p>" + param.name + "</p>");
+                var igroup = $("<div class='input-group'></div>");
+                var input = $("<input type='text class='form-control'>");
+                igroup.append(input);
+                var span = $("<span class=input-group-btn'/>");
+                var btn = $("<button class='btn btn-default btn-sm' type='button'>Go!</button>");
+                newdiv.append(igroup.append(span.append(btn)));
+                ! function outer(pp, bb) {
+                    btn.click(function inner() {
+                        pp.pval = bb.val();
+                        selected_method.ParamChanged(pp);
+                    });
+                }(param, input);
+                break;
+
+            default:
+                console.error("Unkown method parameter type : " + param.name + " - " + param.ptype);
+                break;
+        }
+        pdiv.append(newdiv);
+    }
+    pdiv.hide().show(0);
 }
--- code/method_simple.js
+++ code/method_simple.js
@@ -1,3 +1,28 @@
+//######################################################################
+//########  Interface code, All methods should have this
+/*######################################################################
+method_simple.prototype.Redraw = function (w,h) {};
+method_simple.prototype.ParamChanged = function (param) {};
+method_simple.prototype.SetDateBounds = function (min, max) {};
+method_simple.prototype.SetDate = function (higher,lower) {};
+method_simple.prototype.Update = function () {};
+method_simple.prototype.parameters = [];
+method_simple.prototype.name = "";
+method_simple.prototype.SetData = function (d) {};
+//######################################################################*/
+
+method_simple.prototype.force;
+method_simple.prototype.svg;
+method_simple.prototype.radius = 6;
+method_simple.prototype.link;
+method_simple.prototype.node;
+method_simple.prototype.width = 0;
+method_simple.prototype.height = 0;
+
+method_simple.prototype.minDate;
+method_simple.prototype.maxDate;
+method_simple.prototype.currentDateMin;
+method_simple.prototype.currentDateMax;
 
 function method_simple() {
   // Add object properties like this
@@ -9,49 +34,127 @@ function method_simple() {
     { name: "Test Checkbox2", ptype: "checkbox", pval: true }
   ];
 }
-
-method_simple.prototype.get = function () {
-  alert("Howdy, my name is" + this.name);
+method_simple.prototype.get = function() {
+    alert("Howdy, my name is" + this.name);
 };
 
-//the page has been resized or some other event that requires a redraw
-method_simple.prototype.redraw = function () {
-  alert("Howdy, my name is" + this.name);
+// The page has been resized or some other event that requires a redraw
+method_simple.prototype.Redraw = function(w, h) {
+    if (w !== undefined && h !== undefined) {
+        this.width = w;
+        this.height = h;
+    }
+    //Hate how these are here, move them.
+    // force = customLayout()
+    this.force = d3.layout.force()
+        .gravity(.28)
+        .charge(-640)
+        .linkDistance(50)
+        .size([this.width, this.height]);
+
+   this.svg = d3.select("#chart").append("svg")
+        .attr("width", this.width)
+        .attr("height", this.height);
 };
 
-//called when the user changes any of the Parameters
-method_simple.prototype.paramChanged = function (param) {
-  if (param !== undefined) {
-    var i = this.parameters.indexOf(param);
-    if (i != -1) {
-      console.log("Parameter: " + this.parameters[i].name + " is now: " + this.parameters[i].pval);
+// Called when the user changes any of the Parameters
+method_simple.prototype.ParamChanged = function(param) {
+    if (param !== undefined) {
+        var i = this.parameters.indexOf(param);
+        if (i != -1) {
+            console.log("Parameter: " + this.parameters[i].name + " is now: " + this.parameters[i].pval);
+        } else {
+            console.error("Unkown parameter changed!");
+            console.error(param);
+        }
     } else {
-      console.error("Unkown parameter changed!");
-      console.error(param);
+        //We don't know which parmeter changed, could be more than one. Poll all of them.
+        for (var i in this.parameters) {
+            param = this.parameters[i];
+            console.log("Parameter: " + param.name + " is: " + param.pval);
+        }
     }
-  } else {
-    //We don't know what parmeter changed, could be more than one, poll all of them
-    for (var i in this.parameters) {
-      param = this.parameters[i];
-      console.log("Parameter: " + param.name + " is: " + param.pval);
-    }
-  }
 };
 
-method_simple.prototype.minDate;
-method_simple.prototype.maxDate;
-method_simple.prototype.currentDateMin;
-method_simple.prototype.currentDateMax;
-
-method_simple.prototype.setDateBounds = function (min, max) {
-  this.minDate = min;
-  this.maxDate = max;
+method_simple.prototype.SetDateBounds = function(min, max) {
+    this.minDate = min;
+    this.maxDate = max;
 };
 
-method_simple.prototype.setDate = function (lower, higher) {
-  this.currentDateMin = lower;
-  if(higher === undefined){
-    higher = lower;
-  }
-  this.currentDateMax = higher;
+method_simple.prototype.SetDate = function(higher, lower) {
+    if (lower === undefined) {
+        lower = this.minDate;
+    }
+    this.currentDateMin = lower;
+    this.currentDateMax = higher;
+};
+
+method_simple.prototype.SetData = function(d) {
+    this.data = d;
+};
+
+//######################################################################
+//########    Actual Method code Below
+//######################################################################
+
+//used as callback, needs reference to 'this'
+method_simple.prototype.Tick = function() {
+        selected_method.node.attr("cx", function(d) {
+            return d.x = Math.max(selected_method.radius, Math.min(canvasWidth - selected_method.radius, d.x));
+        })
+        .attr("cy", function(d) {
+            return d.y = Math.max(selected_method.radius, Math.min(canvasHeight - selected_method.radius, d.y));
+        });
+
+        selected_method.link.attr("x1", function(d) {
+            return d.source.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
+};
+
+method_simple.prototype.Update = function() {
+
+    //filter data by date
+    var instance = this; //callback nonsence
+    var filteredLinks = this.data.links.filter(
+        function(d) {
+            return (instance.currentDateMax >= new Date(d.date) && instance.currentDateMin <= new Date(d.date));
+        });
+
+    var fill = d3.scale.category20();
+
+    //Create Links
+    this.link = this.svg.selectAll("line").data(filteredLinks);
+    this.link.enter().append("line")
+        .style("stroke", function(d) {
+            //will break if  > 20 years in scale
+            return d3.rgb(fill(parseInt(d.date.slice(0, 4)) - startDate.getFullYear())).darker();
+        });
+    //when a link is no longer in the set, remove it from the graph.
+    this.link.exit().remove();
+
+    //Create nodes
+    this.node = this.svg.selectAll("circle").data(this.data.nodes);
+    this.node.enter().append("circle")
+        .attr("r", this.radius - .75)
+        .style("fill", function(d) {
+            return fill(d.group);
+        })
+        .style("stroke", function(d) {
+            return d3.rgb(fill(d.group)).darker();
+        })
+        .call(this.force.drag);
+    this.node.exit().remove();
+
+    //restart simulation
+    //force.stop();
+    this.force.nodes(this.data.nodes).links(filteredLinks).on("tick", method_simple.prototype.Tick).start();
 };
